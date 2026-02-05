ac9f765a8947129bc71a67b1fb249b8ea881bdda
adityainnet <adityakumar.innet@gmail.com>
2026-02-05 15:06:36 +0530

    first commit

---
+++ folder-1/app.js
@@ -0,0 +1,7 @@
+// var a = 20;
+// export default a;
+// export const arr = [10, 12, 14, 15];
+export const age = 45;
+export const skills = ["Frontend", "Backend"];
+const user = "Sarthak";
+export default user;
---
+++ folder-1/script.js
@@ -0,0 +1,9 @@
+// var h1 = document.createElement("h1");
+// h1.innerHTML = "hello from js";
+// document.body.appendChild(h1);
+import user from "./app.js";
+import namm from "./test.js";
+import { age, skills } from "./app.js";
+// import { arr } from "./app.js";
+console.log(user, namm);
+console.log(age, skills);
